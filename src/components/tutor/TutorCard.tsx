7c496e4dee355ecaad0f75bd2f5b1c00fc209af7
imon-n <imon.eeecu@gmail.com>
2026-06-16 22:59:20 +0600

    chage at image

--- src/components/tutor/TutorCard.tsx
+++ src/components/tutor/TutorCard.tsx
@@ -31,13 +31,12 @@ export function TutorCard({ tutor }: Props) {
       {/* Image Section */}
       <div className="relative h-[230px] w-full overflow-hidden">
 
-        <Image
-          src={tutor.image}
-          alt={tutor.user.name}
-          fill
+        <img
+              src={tutor?.image}
+              fill
           priority
           className="object-cover object-top transition-transform duration-700 group-hover:scale-110"
-        />
+            />
 
         {/* Dark Overlay */}
         <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/10 to-transparent" />
